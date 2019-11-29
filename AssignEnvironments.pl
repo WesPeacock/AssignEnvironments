3898d0b4048b9f7f8794991c903b7f4ebae89fd0
Wes <wes_peacock@sil.org>
2019-11-29 10:26:19 -0700

    Assign Environments for Affix Allomorphs

--- AssignEnvironments.pl
+++ AssignEnvironments.pl
@@ -361,9 +361,29 @@ while ((my $alloguid, my $allotext) = each (%moaffixallohash)) {
 		say STDERR qq[No environment match for affix allomorph:"$allotext"];
 		next;
 		}
-	say STDERR "TODO put env $envguid into affix allo $alloguid" if $debug;
+#	say STDERR "TODO put env $envguid into affix allo $alloguid" if $debug;
 	# code for Affixes with matched Environments goes here
 	# $envguid is the environment; $alloguid is the matching stem.
+	$allotext =~ m[(.*?)(\ *?)(/.*)];
+	my $trunctext = $1;
+	my $allort = $rthash{$alloguid};
+	# Copy the MorphType node to  make a new tree with PhonEnv node with the environment guid
+	my $XMLstring = ($allort->findnodes('./MorphType'))[0]->toString;
+	$XMLstring =~ s/MorphType/PhoneEnv/g;
+	$XMLstring =~ s/(?<=guid\=\")[^\"]*/$envguid/;
+	# say STDERR "PhoneEnv node:$XMLstring" if $debug;
+	my $newnode = XML::LibXML->load_xml(string => $XMLstring )->findnodes('//*')->[0];
+	$allort->insertAfter($newnode, ($allort->findnodes('./MorphType'))[0]);
+
+	# rewrite Form with the truncated text
+	my ($oldTextnode) = $allort->findnodes('./Form/AUni[@ws="' . $aflang .'"]');
+#		say STDERR "Old envform:", $oldTextnode->toString if $debug;
+#		say STDERR "trunctext:$trunctext" if $debug;
+	$XMLstring = qq[<AUni ws="$aflang">$trunctext</AUni>];
+#		say STDERR "New envform:$XMLstring" if $debug;
+	$newnode = XML::LibXML->load_xml(string => $XMLstring )->findnodes('//*')->[0];
+	$oldTextnode->parentNode->replaceChild($newnode, $oldTextnode) if $oldTextnode;
+
 	}
 
 
